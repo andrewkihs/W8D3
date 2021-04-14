60bf10fdb139caaad2da73490d0b9af4def918a8
Andrew Kihs <ajkihs@gmail.com>
2021-04-14 17:25:02 -0400

    Finish Phase 4

--- intro_javascript_exercises/phase_4_recursion.js
+++ intro_javascript_exercises/phase_4_recursion.js
@@ -19,12 +19,42 @@ function sumRec(arr) {
   return sum;
 }
 
-function exponent(base, exp){
+function exponent_v1(base, exp){
   if(exp == 0){
     return 1;
   }
   return base * exponent(base, exp-1);
 }
-// function fibonacci(n)
-// function deepDup(arr) 
 
+function exponent_v2(base, exp){
+  if (exp == 0){
+    return 1;
+  } 
+  if (exp == 1){
+    return base;
+  }
+  if (exp % 2 == 0){
+    return (exponent_v2(base, exp / 2) ** 2);
+  } else {     
+    return base * (exponent_v2(base, (exp - 1) / 2) ** 2);
+  }
+}
+function fibonacci(n){
+  // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 
+  if (n == 1){ return [0]; }
+  if (n == 2){ return [0, 1]; }
+
+  let fibArr = fibonacci(n-1);
+  fibArr.push(fibArr[fibArr.length-1] + fibArr[fibArr.length-2]);
+  return fibArr; 
+}
+
+
+function deepDup(arr){
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] instanceof Array){
+      arr[i] = deepDup(arr[i]);
+    }
+  }
+  return arr;
+}
